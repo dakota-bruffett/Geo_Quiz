eaeb5af9b0413121b1afcac8583876577a7d1f67
nq2623up <DakotaBruffett@gmail.com>
2023-02-09 15:53:06 -0600

    Comment Chapter

--- app/src/main/java/com/example/geo_quiz/MainActivity.kt
+++ app/src/main/java/com/example/geo_quiz/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.geo_quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Truth_Button: Button
+    private  lateinit var False_button: Button
+    private lateinit var Question:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+        Truth_Button = findViewById(R.id.Truth_button)
+        False_button = findViewById(R.id.False_button)
+        Question = findViewById(R.id.Question)
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+
+    }
+    private const val TAG = "QuizViewModel"
+    class QuizViewModel : ViewModel(){
+        init {
+            Log.d(TAG, "ViewModel instance Created")
+        }
+
+        override fun onCleared() {
+            super.onCleared()
+            Log.d(TAG, "ViewModel Instance about to be destroyed" )
+        }
+        private val  questionBank = listOf<>(
+            Question(R.id.Question, true),
+            Question(R.id.Question, true),
+            Question(R.id.Question, true),
+            Question(R.id.Question, true),
+            Question(R.id.Question, true),
+
+        )
     }
 }
